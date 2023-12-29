3cfcd87927fe08a03157bebed4d0012b4446cec3
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-30 02:06:31 +0900

    Update ex03.js

--- ex03.js
+++ ex03.js
@@ -1,2 +1,2 @@
-function irorennsyuu1(){document.getElementById('irorennsyuui').src='image/色練習1.jpg';}
+function irorennsyuu1(){document.getElementById('irorennsyuu').src='image/色練習1.jpg';}
 function irorennsyuu2(){document.getElementById('irorennsyuu').src='image/色練習2.jpg';}
