190ea61b1c80a92fdfc3b5eccea938b6042f08a6
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-30 01:02:48 +0900

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -1,2 +1,6 @@
 function gazou1(){document.getElementById('rennsyuui').src='image/kisorenn1.jpg';}
 function gazou2(){document.getElementById('rennsyuu2').src='image/kisorenn4.jpg';}
+
+
+function gazou1(){document.getElementById('neko').src='image/animal_cat_kowai.png';}
+function gazou2(){document.getElementById('neko').src='image/cat_koubakozuwari_brown.png';}
