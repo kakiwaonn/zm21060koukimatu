9d727caa755c053b2434a49d30b7916718a72109
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-30 17:26:15 +0900

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -1,6 +1,15 @@
 function rennsyuu2(){document.getElementById('rennsyuu').src='image/kisorenn4.jpg';}
 function rennsyuu3(){document.getElementById('rennsyuu').src='image/kisorenn1.jpg';}
 
+function kuukannhako1(){document.getElementById('kuukannhako').src='image/空間箱4.jpg';}
+function kuukannhako2(){document.getElementById('kuukannhako').src='image/空間箱1.jpg';}
+
+
+
+
+
+
+
 
 /* function imgchange3(){document.getElementById('logo2').src='image/josai_logo.jpg';}
 function imgchange4(){document.getElementById('logo2').src='image/png-transparent-josai-university-hd-logo.png';} */
