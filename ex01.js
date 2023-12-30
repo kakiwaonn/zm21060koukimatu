a786bf04b12c84a7f74a3df89c8a05e2e27fc476
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-30 17:35:14 +0900

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -4,5 +4,12 @@ function rennsyuu3(){document.getElementById('rennsyuu').src='image/kisorenn1.jp
 
 
 
+
+
+
+
+
+
+
 /* function imgchange3(){document.getElementById('logo2').src='image/josai_logo.jpg';}
 function imgchange4(){document.getElementById('logo2').src='image/png-transparent-josai-university-hd-logo.png';} */
