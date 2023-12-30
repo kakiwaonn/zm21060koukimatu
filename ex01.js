c04d2b0d3ced97b1e049aca366ada63abcd41746
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-30 15:37:13 +0900

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -1,6 +1,6 @@
-function rennsyuu2(){document.getElementById('rennsyuu').src='image/kisorenn1.jpg';}
-function rennsyuu3(){document.getElementById('rennsyuu').src='image/kisorenn4.jpg';}
+function rennsyuu2(){document.getElementById('rennsyuu').src='image/kisorenn4.jpg';}
+function rennsyuu3(){document.getElementById('rennsyuu').src='image/kisorenn1.jpg';}
 
 
-function imgchange3(){document.getElementById('logo2').src='image/josai_logo.jpg';}
-function imgchange4(){document.getElementById('logo2').src='image/png-transparent-josai-university-hd-logo.png';}
+<!--function imgchange3(){document.getElementById('logo2').src='image/josai_logo.jpg';}
+function imgchange4(){document.getElementById('logo2').src='image/png-transparent-josai-university-hd-logo.png';}-->
