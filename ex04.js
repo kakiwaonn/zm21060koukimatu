9f05e773af217c891ae82d944766baa9bc3872dc
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-30 16:37:49 +0900

    Update ex04.js

--- ex04.js
+++ ex04.js
@@ -1,4 +1,4 @@
-function irasuto1(){document.getElementById('irasuto').src='image/イラスト2.png' width="1000" height="550";}
-function irasuto2(){document.getElementById('irasuto').src='image/イラスト.png' width="500" height="750";}
+function irasuto1(){document.getElementById('irasuto').src='image/イラスト2.png' width="1000" height="550" alt="1";}
+function irasuto2(){document.getElementById('irasuto').src='image/イラスト.png' width="500" height="750" alt="2";}
 
 /*<img src="image/ｈｐ１.png" width="400" height="150" alt="１"><br>*/
