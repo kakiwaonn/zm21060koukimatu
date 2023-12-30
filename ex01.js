1d355ae62c80a99baf8f3b75cfacca235cb2f1f2
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-30 15:50:08 +0900

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -2,5 +2,5 @@ function rennsyuu2(){document.getElementById('rennsyuu').src='image/kisorenn4.jp
 function rennsyuu3(){document.getElementById('rennsyuu').src='image/kisorenn1.jpg';}
 
 
-<!--function imgchange3(){document.getElementById('logo2').src='image/josai_logo.jpg';}
-function imgchange4(){document.getElementById('logo2').src='image/png-transparent-josai-university-hd-logo.png';}-->
+/* function imgchange3(){document.getElementById('logo2').src='image/josai_logo.jpg';}
+function imgchange4(){document.getElementById('logo2').src='image/png-transparent-josai-university-hd-logo.png';} */
