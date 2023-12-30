6debfbe7f39150d7b9ae938bb1f4a379666dcec6
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-30 17:31:50 +0900

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -1,17 +1,6 @@
 function rennsyuu2(){document.getElementById('rennsyuu').src='image/kisorenn4.jpg';}
 function rennsyuu3(){document.getElementById('rennsyuu').src='image/kisorenn1.jpg';}
 
-function kuukannhako1(){document.getElementById('kuukannhako').src='image/空間箱4.jpg';}
-function kuukannhako2(){document.getElementById('kuukannhako').src='image/空間箱1.jpg';}
-
-function irorennsyuu1(){document.getElementById('irorennsyuu').src='image/色練習2.jpg';}
-function irorennsyuu2(){document.getElementById('irorennsyuu').src='image/色練習1.jpg';}
-
-function irasuto1(){document.getElementById('irasuto').src={'image/イラスト2.png' idth="1000" height="550"};}
-function irasuto2(){document.getElementById('irasuto').src={'image/イラスト.png' width="500" height="750"};}
-
-
-
 
 
 
